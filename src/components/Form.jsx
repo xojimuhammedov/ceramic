3d675f810b815d2e423428a1f8918c1806d99f5e
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2024-11-09 15:30:14 +0500

    add

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,79 +6,176 @@ import {
   SimpleGrid,
   Text,
 } from "@chakra-ui/react";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { toast } from "react-toastify";
 
 function Form() {
   const { t } = useTranslation();
+  const [nameValue, setNameValue] = useState("");
+  const [numberValue, setNumberValue] = useState("");
+  const [textValue, setTextValue] = useState("");
+  const [company, setCompany] = useState("");
+  const [country, setCountry] = useState("");
+  const [location, setLocation] = useState("");
+
+  function changeNumber(item) {
+    setNumberValue(item);
+  }
+
+  function changeName(item) {
+    setNameValue(item);
+  }
+  function changeText(item) {
+    setTextValue(item);
+  }
+  function changeCompany(item) {
+    setCompany(item);
+  }
+  function changeCountry(item) {
+    setCountry(item);
+  }
+  function changeLocation(item) {
+    setLocation(item);
+  }
+
+  const handleClear = () => {
+    setNameValue("");
+    setNumberValue("");
+    setTextValue("");
+    setCompany("");
+    setCountry("");
+    setLocation("");
+  };
+  let bot = {
+    TOKEN: "8050907392:AAGe_5c4l2KyI2l1cm9WM-oad3totFzUeVg",
+    chatID: "-1002323257681",
+    message: `
+          Assalomu alaykum sizga yangi xabar!%0A
+          %0AIsmi 👤: ${nameValue}; 
+          %0ATelefon raqami ☎: ${numberValue};
+          %0APochtasi ☎: ${textValue};
+          `,
+  };
+
+  function sendMessage(e) {
+    e.preventDefault();
+
+    fetch(
+      `https://api.telegram.org/bot${bot.TOKEN}/sendMessage?chat_id=${bot.chatID}&text=${bot.message} `,
+      {
+        method: "GET",
+      }
+    ).then(
+      () => {
+        handleClear();
+        // window.location.reload();
+        toast.success("Sizning xabaringiz muvaffaqiyatli yuborildi!");
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
   return (
     <Box>
       <Box className="container">
-          <Box {...css.form}>
-            <Box className="container-mix">
-              <Heading {...css.title}>
-                {t("Here you get all our attention")}
-              </Heading>
-              <Text {...css.text}>{t("contactText")}</Text>
+        <Box {...css.form}>
+          <Box className="container-mix">
+            <Heading {...css.title}>
+              {t("Here you get all our attention")}
+            </Heading>
+            <Text {...css.text}>{t("contactText")}</Text>
 
-              <Text m={"24px 0"} {...css.text}>
-                {t("Do you have any questions or suggestions?")}
-              </Text>
-              <Text mb={"24px"} {...css.text}>
-                {t("contactText1")}
-              </Text>
+            <Text m={"24px 0"} {...css.text}>
+              {t("Do you have any questions or suggestions?")}
+            </Text>
+            <Text mb={"24px"} {...css.text}>
+              {t("contactText1")}
+            </Text>
 
-              <form action="">
-                <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} gap="24px">
-                  <label className="form-label" htmlFor="">
-                    {t("First name*")}
-                    <Input {...css.input} type="text" />
-                  </label>
-                  <label className="form-label" htmlFor="">
-                    {t("Last name*")}
-                    <Input {...css.input} type="text" />
-                  </label>
-                  <label className="form-label" htmlFor="">
-                    {t("Company*")}
-                    <Input {...css.input} type="text" />
-                  </label>
-                  <label className="form-label" htmlFor="">
-                    {t("email*")}
-                    <Input {...css.input} type="email" />
-                  </label>
-                  <label className="form-label" htmlFor="">
-                    {t("Street")}
-                    <Input {...css.input} type="text" />
-                  </label>
-                  <label className="form-label" htmlFor="">
-                    {t("Location / Town")}
-                    <Input {...css.input} type="text" />
-                  </label>
-                  <label className="form-label" htmlFor="">
-                    {t("Country")}
-                    <Input {...css.input} type="text" />
-                  </label>
-                  <label className="form-label" htmlFor="">
-                    {t("Phone number")}
-                    <Input {...css.input} type="text" />
-                  </label>
-                </SimpleGrid>
-                <Button type="submit" {...css.button}>
-                  {t("Submit")}
-                </Button>
-              </form>
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001.4935650251427!2d69.24745957586927!3d41.21101347132282!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae60e8bb54cecb%3A0xef1a6253f161df48!2sNilufar%20St%2C%20Tashkent%2C%20Uzbekistan!5e0!3m2!1sen!2s!4v1730994409889!5m2!1sen!2s"
-                // width="600"
-                // height="450"
-                // style="border:0;"
-                style={{ border: 0 }}
-                allowfullscreen=""
-                className="form-map"
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-              />
-            </Box>
+            <form action="">
+              <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} gap="24px">
+                <label className="form-label" htmlFor="name">
+                  {t("First name*")}
+                  <Input
+                    value={nameValue}
+                    onChange={(e) => changeName(e.target.value)}
+                    {...css.input}
+                    name="name"
+                    type="text"
+                  />
+                </label>
+                <label className="form-label" htmlFor="">
+                  {t("Company*")}
+                  <Input
+                    value={company}
+                    onChange={(e) => changeCompany(e.target.value)}
+                    {...css.input}
+                    type="text"
+                  />
+                </label>
+                <label className="form-label" htmlFor="">
+                  {t("email*")}
+                  <Input
+                    value={textValue}
+                    onChange={(e) => changeText(e.target.value)}
+                    {...css.input}
+                    type="email"
+                  />
+                </label>
+                <label className="form-label" htmlFor="">
+                  {t("Location / Town")}
+                  <Input
+                    value={location}
+                    onChange={(e) => changeLocation(e.target.value)}
+                    {...css.input}
+                    type="text"
+                  />
+                </label>
+                <label className="form-label" htmlFor="">
+                  {t("Country")}
+                  <Input
+                    value={country}
+                    onChange={(e) => changeCountry(e.target.value)}
+                    {...css.input}
+                    type="text"
+                  />
+                </label>
+                <label className="form-label" htmlFor="">
+                  {t("Phone number")}
+                  <Input
+                    value={numberValue}
+                    onChange={(e) => changeNumber(e.target.value)}
+                    {...css.input}
+                    type="text"
+                  />
+                </label>
+              </SimpleGrid>
+              <Button type="submit" onClick={sendMessage} {...css.button}>
+                {t("Submit")}
+              </Button>
+            </form>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6002.6404858922715!2d69.2501422883606!3d41.21479120205732!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae61badf20ac1d%3A0x9a5393ee55928047!2sORIENT%20CERAMIC!5e0!3m2!1sen!2sus!4v1731148173336!5m2!1sen!2sus"
+              style={{ border: 0 }}
+              allowfullscreen=""
+              className="form-map"
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade" />
+            {/* <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001.4935650251427!2d69.24745957586927!3d41.21101347132282!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae60e8bb54cecb%3A0xef1a6253f161df48!2sNilufar%20St%2C%20Tashkent%2C%20Uzbekistan!5e0!3m2!1sen!2s!4v1730994409889!5m2!1sen!2s"
+              // width="600"
+              // height="450"
+              // style="border:0;"
+              style={{ border: 0 }}
+              allowfullscreen=""
+              className="form-map"
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            /> */}
           </Box>
+        </Box>
       </Box>
     </Box>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import App from "./App.jsx";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import "./i18n.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <ToastContainer />
     <BrowserRouter>
       <ChakraProvider value={defaultSystem}>
         <Suspense fallback="loading">
